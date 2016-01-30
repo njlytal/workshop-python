26f019bed454a7c2e7a2616cbb4210e1a7d90694
Nick Lytal <njlytal@email.arizona.edu>
2016-01-30 16:23:02 -0700

    Initial commit - Day 1 files

---
+++ conditional.py
@@ -0,0 +1,9 @@
+x = 17
+day = "Sunday"
+if not (x > 10):
+    print("The day is " + str(x) + 
+          ". What a lovely day!")
+elif day == "Saturday" or "Sunday":
+    print("I enjoy the weekends! " + day + "s are fun!")
+else:
+    print("I don't think this will work...")
---
+++ iteration.py
@@ -0,0 +1,9 @@
+books = ["Tale of Two Cities", "Harry Potter", "Lord of the Rings"]
+nums = range(5)
+
+for b in books:
+    print(b)
+    
+for n in nums:
+    print(n)
+    
